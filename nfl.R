ec4543d209f0c5b1f2dd1486cdb3199a5de8ad9f
Joel Liebert <joelmliebert@gmail.com>
2021-12-16 12:15:34 -0500

    added 10 seasons, cleaned up plot

--- nfl.R
+++ nfl.R
@@ -5,7 +5,17 @@ library(nflfastR)
 
 options(scipen = 9999)
 
-data <- readRDS(url('https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/data/play_by_play_2019.rds'))
+# data <- readRDS(url('https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/data/play_by_play_2019.rds'))
+
+season_txt <- "2010 - 2019"
+seasons <- 2010:2019
+data <- map_df(seasons, function(x) {
+  readRDS(
+    url(
+      paste0("https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/data/play_by_play_",x,".rds")
+    )
+  )
+})
 
 head(data)
 sum(!is.na(data$td_team))
@@ -32,10 +42,15 @@ after_turnover_drives <- drive_touchdowns[which(drive_touchdowns$drive_after_tur
   summarize(td_percentage = mean(touchdown))
 
 ggplot(baseline_drives) +
-  geom_col(aes(starting_position, td_percentage), fill = "#5778a4") +
-  geom_col(data = after_turnover_drives, aes(starting_position, td_percentage), fill = "#e49444", width = 0.5) +
+  geom_col(aes(starting_position, td_percentage, fill = "#5778a4")) +
+  geom_col(data = after_turnover_drives, aes(starting_position, td_percentage, fill = "#e49444"), width = 0.5) +
+  ggtitle(paste0("Touchdown drives following forced turnovers vs. all drives, ", season_txt)) +
+  labs(x = "Starting yard line", y = "Proportion of drives ending in touchdowns") +
+  scale_fill_manual(name="Touchdown Percentage", 
+                    values=c("#5778a4", "#e49444"), 
+                    labels=c("All drives", "Drives following forced turnover")) + 
+  scale_x_discrete(labels = c('1-10','11-20','21-30','31-40','41-50','51-60','61-70','71-80','81-90','91-100')) +
   theme_classic() +
-  labs(x = "Starting 10-yard line", y = "Proportion of drives ending in touchdowns") +
-  ggtitle("Touchdown drives after turnovers vs baseline") +
+  theme(legend.position = c(0.8, 0.85)) +
   coord_cartesian(expand = FALSE)
-                  
+  
