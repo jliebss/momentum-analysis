f9e4d31da2ce7fd098444e7fbb762e9b2e0441cf
Joel Liebert <joelmliebert@gmail.com>
2021-12-17 23:45:13 -0500

    added duplicate cfb plot; failed at turning nfl plot into dot plot with mean/stdev line, but kept code in comments

---
+++ cfb.R
@@ -0,0 +1,43 @@
+pacman::p_load(tidyverse, cfbfastR, zoo, ggimage, gt)
+
+season_txt <- "2014-2019"
+seasons <- 2014:2019
+
+tictoc::tic()
+pbp <- data.frame()
+pbp <- cfbfastR::load_cfb_pbp(seasons)
+tictoc::toc()
+# pbp <- cfbfastR::load_cfb_pbp(2019)
+
+pbp$drive_after_turnover = lag(pbp$turnover, n=1)
+
+cfb_drive_touchdowns <- pbp[,c("drive_id","drive_start_yards_to_goal","turnover","touchdown")] %>% 
+  mutate(starting_position = factor(ceiling(drive_start_yards_to_goal / 10), levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")),
+         drive_after_turnover = lag(turnover)) %>% 
+  group_by(drive_id, starting_position) %>%
+  summarize(drive_after_turnover = sum(drive_after_turnover),
+            touchdown = sum(touchdown))
+cfb_drive_touchdowns <- cfb_drive_touchdowns[complete.cases(cfb_drive_touchdowns), ]
+
+cfb_baseline_drives <- cfb_drive_touchdowns %>% 
+  group_by(starting_position) %>% 
+  summarize(td_percentage = mean(touchdown))
+cfb_after_turnover_drives <- cfb_drive_touchdowns[which(cfb_drive_touchdowns$drive_after_turnover == 1), ] %>% 
+  group_by(starting_position) %>% 
+  summarize(td_percentage = mean(touchdown))
+
+ggplot(cfb_baseline_drives) +
+  geom_col(aes(starting_position, td_percentage, fill = "#5778a4")) +
+  geom_col(data = cfb_after_turnover_drives, aes(starting_position, td_percentage, fill = "#e49444"), width = 0.5) +
+  labs(title   = "Are CFB drives following forced turnovers more likely to score touchdowns?",
+       x       = "Starting yard line",
+       y       = "Percentage of drives ending in touchdowns",
+       caption = paste0("Data from ", season_txt, " CFB seasons")) +
+  scale_fill_manual(name    = "", 
+                    values  = c("#5778a4", "#e49444"), 
+                    labels  = c("All drives", "Drives following forced turnover")) +
+  scale_x_discrete(labels   = c('1-10','11-20','21-30','31-40','41-50','51-60','61-70','71-80','81-90','91-100')) +
+  scale_y_continuous(labels = scales::percent) +
+  theme_classic() +
+  theme(legend.position  = c(0.8, 0.85)) +
+  coord_cartesian(expand = FALSE)
--- nfl.R
+++ nfl.R
@@ -27,10 +27,9 @@ data$turnover <- 0
 data$turnover[which(data$interception == 1 | data$fumble_lost == 1)] <- 1
 data$drive_after_turnover <- lag(data$turnover, n = 1)
 
-# TODO: Verify starting field position works correctly
 # Groups each drive and provides its starting field position (in ten-yard increments),
 # whether it follows a forced turnover, and if it results in a touchdown
-drive_touchdowns <- data[data$special == 0, ] %>% 
+drive_touchdowns <- data[data$special == 0, ] %>%
   group_by(game_id, drive) %>%
   mutate(starting_position    = factor(ceiling(first(yardline_100) / 10), levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")),
          drive_after_turnover = first(drive_after_turnover)) %>% 
@@ -50,7 +49,7 @@ after_turnover_drives <- drive_touchdowns[which(drive_touchdowns$drive_after_tur
 ggplot(baseline_drives) +
   geom_col(aes(starting_position, td_percentage, fill = "#5778a4")) +
   geom_col(data = after_turnover_drives, aes(starting_position, td_percentage, fill = "#e49444"), width = 0.5) +
-  labs(title   = "Are drives following forced turnovers more likely to score touchdowns?",
+  labs(title   = "Are NFL drives following forced turnovers more likely to score touchdowns?",
        x       = "Starting yard line",
        y       = "Percentage of drives ending in touchdowns",
        caption = paste0("Data from ", season_txt, " NFL seasons")) +
@@ -62,5 +61,27 @@ ggplot(baseline_drives) +
   theme_classic() +
   theme(legend.position  = c(0.8, 0.85)) +
   coord_cartesian(expand = FALSE)
-  
-# dummy line - delete this
+
+
+
+# Maybe come back to this
+# temp <- data[, c("game_id", "drive", "desc", "drive_start_yard_line", "drive_end_yard_line","yardline_100", "touchdown")]
+# 
+# drive_data <- data[which(data$play_type == "pass" | data$play_type == "run"), ] %>%
+#   group_by(game_id, drive) %>%
+#   mutate(drive_yards_to_go = first(yardline_100),
+#          drive_final_yard_line = last(yardline_100),
+#          drive_percent_yards_gained = (drive_yards_to_go - drive_final_yard_line) / drive_yards_to_go) %>% 
+#   group_by(game_id, drive, drive_yards_to_go, drive_final_yard_line, drive_percent_yards_gained, drive_after_turnover) %>%
+#   summarize(touchdown = sum(td)) %>% 
+#   mutate(decile_yard_line = factor(ceiling(drive_yards_to_go / 10), levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")),
+#          drive_percent_yards_gained = ifelse(touchdown == 1, 1, drive_percent_yards_gained))
+# 
+# ggplot(drive_data, aes(decile_yard_line, drive_percent_yards_gained, color = drive_after_turnover)) +
+#   # geom_jitter(alpha = 0.2, width = 0.1) +
+#   geom_point(aes(decile_yard_line, mean(drive_percent_yards_gained), color = drive_after_turnover))
+# 
+# 
+# stat_summary(fun.y = mean,
+#              geom = "point",
+#              color = "red")
